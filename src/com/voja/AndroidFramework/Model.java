a5b445c46c87355280ea76330372b332f2344cdf
Voislav Jovanovic <voislavj@gmail.com>
2013-03-28 20:32:15 +0100

    Init Repo (android sdk-v7)

---
+++ src/com/voja/AndroidFramework/Model.java
@@ -0,0 +1,60 @@
+package com.voja.AndroidFramework;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import android.content.Context;
+
+public class Model {
+	
+	SQLiteHelper connection;
+	Context context;
+	
+	ArrayList<String> sqls = new ArrayList<String>();
+	
+	public Model(Context c) {
+		context = c;
+		
+		sqls = loadStructure();
+		connection = new SQLiteHelper(context, sqls);
+		connection.open();
+	}
+	
+	public ArrayList<String> loadStructure() {
+		ArrayList<String> sqls = new ArrayList<String>();
+		String sql 	 = "";
+		try {
+			InputStream in = context.getAssets().open("database.sql");
+			BufferedReader br = new BufferedReader(new InputStreamReader(in));
+			
+			String line;
+			while((line = br.readLine()) != null) {
+				sql += line;
+			}
+			
+			String rgx = "create.*?;|(update.*?;)|(insert.*?;)";
+			Pattern pattern = Pattern.compile(rgx, Pattern.CASE_INSENSITIVE | Pattern.DOTALL | Pattern.MULTILINE);
+			Matcher matcher = pattern.matcher(sql);
+			
+			while (matcher.find()) {
+				sqls.add(matcher.group());
+			}
+		} catch(IOException e) {
+			android.util.Log.e(context.getPackageName(), "Error loading database structure. " + e.getMessage());
+		}
+		return sqls;
+	}
+	
+	public SQLiteHelper getConnection() {
+		return connection;
+	}
+	
+	public Context getContext() {
+		return this.context;
+	}
+}
---
+++ src/com/voja/AndroidFramework/SQLiteHelper.java
@@ -0,0 +1,51 @@
+package com.voja.AndroidFramework;
+
+import java.util.ArrayList;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class SQLiteHelper extends SQLiteOpenHelper {
+	
+	public static final String DB_NAME    = "schedule";
+	public static final int    DB_VERSION = 1;
+	
+	SQLiteDatabase db;
+	Context context;
+	
+	ArrayList<String> sqls;
+
+	public SQLiteHelper(Context c, ArrayList<String> createSQLs) {
+		super(c, DB_NAME, null, DB_VERSION);
+		
+		context = c;
+		sqls = createSQLs;
+		
+		open();
+	}
+	
+	@Override
+	public void onCreate(SQLiteDatabase database) {
+		for (int i=0; i<sqls.size(); i++) {
+			database.execSQL(sqls.get(i));
+		}
+	}
+	
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		onCreate(db);
+	}
+	
+	public void open() {
+		db = getWritableDatabase();
+	}
+	
+	public Cursor query(String sql) {
+		return db.rawQuery(sql, null);
+	}
+	public Cursor query(String sql, String[] selArgs) {
+		return db.rawQuery(sql, selArgs);
+	}
+}
